d567733a62b8c146981ccdac0e9ed5cd956c8d52
jxunn <jxun@umich.edu>
2024-09-16 23:30:46 -0400

    Co-authored-by: Tisya Mukherjee <tisyamukherjee@users.noreply.github.com>

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary fixed-top">
+      <Container>
+        <Navbar.Brand href="#home">GreenScan</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
